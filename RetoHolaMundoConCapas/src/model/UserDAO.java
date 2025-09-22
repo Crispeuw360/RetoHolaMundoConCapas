063a69fb97469cc806858cfd33bf1c81cc79cf1f
Crispeuw360 <igornikolaidis@gmail.com>
2025-09-22 08:20:18 +0200

    actualizar

--- RetoHolaMundoConCapas/src/model/UserDAO.java
+++ RetoHolaMundoConCapas/src/model/UserDAO.java
@@ -9,11 +9,6 @@ package model;
  *
  * @author 2dami
  */
-<<<<<<< HEAD
-public class Controller {
-    
-=======
 public interface UserDAO {
         public abstract void example_method();
->>>>>>> 04141d1140a9e629a9cb1ac8d398d0bfd188a67e
 }
